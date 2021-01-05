7bcdef9ac6b6dbd3f29888dd28d00209bad0037a
c3rb1n <ilya_prudnikov97@mail.ru>
2021-01-05 20:49:18 +0300

    add solution for 1 exercise for 8 kyu

---
+++ 8_kyu/Switch it Up!/main.ts
@@ -0,0 +1,4 @@
+export const switchItUp = (intNumber: number): string => ["Zero", "One", "Two",
+                                                          "Three", "Four", "Five",
+                                                          "Six", "Seven", "Eight",
+                                                          "Nine"][intNumber];
