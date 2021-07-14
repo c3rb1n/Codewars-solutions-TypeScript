cdfb7f415db5fcfafdf54de3908efc173a5a5fab
c3rb1n <ilya_prudnikov97@mail.ru>
2021-07-14 20:32:31 +0300

    renaming

--- 6_kyu/Find the odd int/main.ts
+++ 6_kyu/Find the odd int/main.ts
@@ -2,7 +2,7 @@ export const findOdd = (xs: number[]): number | undefined => {
     for (let i: number = 0; i < xs.length; i++) {
         let elCounter: number = 0;
       
-        xs.forEach((a: number): boolean | number => a === xs[i] && elCounter++); 
+        xs.forEach((el: number): boolean | number => el === xs[i] && elCounter++); 
       
         if (elCounter % 2 !== 0) return xs[i];
     }
